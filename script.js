a952402fc01cfcb7b91119937337e4b1588e92b3
Quoc Tuan <you@example.com>
2022-01-21 10:44:17 +0700

    switch player

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 // Select Element
+const player0Ele = document.querySelector('.player--0');
+const player1Ele = document.querySelector('.player--1');
 const score0Ele = document.querySelector('#score--0');
 const score1Ele = document.getElementById('score--1');
 const current0Ele = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0Ele.textContent = 0;
 score1Ele.textContent = 0;
 diceEle.classList.add('hidden');
 
-let currentScore = 0
+const scores = [0,0]; // Final score
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice Function
 btnRoll.addEventListener('click', function() {
@@ -30,8 +34,13 @@ btnRoll.addEventListener('click', function() {
   // 3. Check for roll = 1 
   if(dice !== 1) {
     currentScore += dice;
-    current0Ele.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-
+    // Switch player 
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Ele.classList.toggle('player--active');
+    player1Ele.classList.toggle('player--active');
   }
 })
